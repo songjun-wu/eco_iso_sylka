ea27fd53a5256177b64632ab947d937b52dc0d28
(no author) <noauthor@umontana.edu>
2013-06-20 22:06:52 +0000

    -Fixed bug in the calculation of rexp. vt was in the numerator instead of the denominator due to missing parentheses

--- Radiation/CalcAerodynResist.cpp
+++ Radiation/CalcAerodynResist.cpp
@@ -32,10 +32,10 @@ REAL8 Basin::CalcAerodynResist(REAL8 u_za, REAL8 z_a, REAL8 z_0u, REAL8 z_du, RE
 		vt = u_za * vonkarman * vonkarman * (Ht - z_do) / log( (z_a - z_do)/z_0o ); // conductance at the top of the tree, which is the bottom of the overstory wind log profile
 		a = min<double>(max<double>(1.0,0.2 * LAI * Ht / lm), 4); //wind extinction factor as per foken 2008, Campbell and Norman 1998
 
-		rexp = (1 / a * vt) * Ht * expl(a) * ( expl(-a * ( zt ) / Ht) - expl(-a * (z_do + z_0o) / Ht));
+		rexp = (1 / (a * vt) ) * Ht * expl(a) * ( expl(-a * ( zt ) / Ht) - expl(-a * (z_do + z_0o) / Ht));
 		rlog_o = rlog(u_za, z_a, z_do, z_0o, option); // add the resistance due to the logarithmic profile over the
 
-		rlog_u=0; //for the time being the exponential profile goes all the way down to 0 velocity (zt = z_du+z_0u) for surface
+		//rlog_u=0; //for the time being the exponential profile goes all the way down to 0 velocity (zt = z_du+z_0u) for surface
 
 	}
 	//otherwise resistance is simply the aerodynamic resistance associated with a logarithmic wind profile
