d780d9cce5f6a593e0768cdad20d3a4f0c27bf16
Marco Maneta <marco.maneta@umontana.edu>
2015-03-16 14:43:38 -0600

    k# On branch InfiltRichHead

--- Hydro/Infilt_Richards.cpp
+++ Hydro/Infilt_Richards.cpp
@@ -41,8 +41,7 @@ void Basin::Infilt_Richards(double &f, double &F, double &theta, double &theta1,
 
 	//Unsaturated Hydr Cond
 	double K1=0, K12=0, K23=0, K3=0;
-	//Soil suction head
-	double psi1=0, psi2=0, psi3=0;
+
 
 
 	//derivatives of relative saturation with respect to soil tension
@@ -83,6 +82,8 @@ void Basin::Infilt_Richards(double &f, double &F, double &theta, double &theta1,
    x[1] = psiae*powl(S2,-lam);
    x[2] = psiae*powl(S3,-lam);
 
+   double initstor = theta1*d1 + theta2*d2 + theta3*d3;
+
 	int k = 0;
    do{
 
@@ -91,12 +92,14 @@ void Basin::Infilt_Richards(double &f, double &F, double &theta, double &theta1,
 		K23 = Ks/(d2+d3) * (d2*powl(S2,p) + d3*powl(S3,p)  );
 		K3 = Ks * powl(S3,p);
 
-		infilt = std::min<double> (K1*(1 + (psi1 + pond)/D1 ), pond*invdt );
+		infilt = std::min<double> (K1*(1 + (x[0] + pond)/D1 ), pond*invdt );
+		if(S1==1)
+			infilt=0;
 		Qout = K3*d3dxslope;
 
-		Fun[0] = d1*(theta1*invdt) - d1*(theta11*invdt) + infilt - K12*(1 + (psi2 - psi1)/D2 ) ;
-		Fun[1] = d2*(theta2*invdt) - d2*(theta21*invdt) + K12*(1 + (psi2 - psi1)/D2 ) - K23*(1 + (psi3 - psi2)/D3 );
-		Fun[2] = d3*(theta3*invdt) - d3*(theta31*invdt) + K23*(1 + (psi3 - psi2)/D3 )+ Qin - Qout - d3*S3*L;
+		Fun[0] = d1*(theta1*invdt) - d1*(theta11*invdt) + infilt - K12*(1 + (x[1] - x[0])/D2 ) ;
+		Fun[1] = d2*(theta2*invdt) - d2*(theta21*invdt) + K12*(1 + (x[1] - x[0])/D2 ) - K23*(1 + (x[2] - x[1])/D3 );
+		Fun[2] = d3*(theta3*invdt) - d3*(theta31*invdt) + K23*(1 + (x[2] - x[1])/D3 )+ Qin - Qout - d3*S3*L;
 
 		dS1dpsi1 = x[0]<psiae ? 0 : -powl(psiae/x[0],1/lam)/(lam*x[0]);
 		dS2dpsi2 = x[1]<psiae ? 0 : -powl(psiae/x[1],1/lam)/(lam*x[1]);
@@ -109,36 +112,34 @@ void Basin::Infilt_Richards(double &f, double &F, double &theta, double &theta1,
 		dK23dpsi3 = x[2]<psiae ? 0 : Ks*d3/(d2+d3)*p*powl(S3,p-1)*dS3dpsi3;
 		dK3dpsi3  = x[2]<psiae ? 0 : Ks*p*powl(S3,p-1)*dS3dpsi3;
 
-		if(infilt < K1*(1 + (psi1 + pond)/D1 ))
+		if(infilt < K1*(1 + (x[0] + pond)/D1 ) || infilt ==0)
 			dinfiltdpsi1 = 0;
 		else
-			dinfiltdpsi1 = dK1dpsi1*(1 + (psi1 + pond)/D1) + (K1/D1);
+			dinfiltdpsi1 = dK1dpsi1*(1 + (x[0] + pond)/D1) + (K1/D1);
 
 
 		// Fill the Jacobian
-		J(0,0) = -d1*invdt*(thetas-thetar)*dS1dpsi1 + dinfiltdpsi1 - dK12dpsi1*(1 + (psi2 - psi1)/D2 ) +(K12/D2);
-		J(0,1) = -dK12dpsi2*( 1 + (psi2 - psi1)/D2 ) - (K12/D2);
+		J(0,0) = -d1*invdt*(thetas-thetar)*dS1dpsi1 + dinfiltdpsi1 - dK12dpsi1*(1 + (x[1] - x[0])/D2 ) +(K12/D2);
+		J(0,1) = -dK12dpsi2*( 1 + (x[1] - x[0])/D2 ) - (K12/D2);
 		//J(0,2) = 0; // Just to remember that this is element of the Jacobian is zero
-		J(1,0) = dK12dpsi1*( 1 + (psi2 - psi1)/D2 ) - (K12/D2);
-		J(1,1) = -d2*invdt*(thetas-thetar)*dS2dpsi2 + dK12dpsi2*(1+(psi2-psi1)/D2) + (K12/D2) - dK23dpsi2*(1+(psi3-psi2)/D3)+(K23/D3);
-		J(1,2) = -dK23dpsi3*(1+(psi3-psi2)/D3)-(K23/D3);
+		J(1,0) = dK12dpsi1*( 1 + (x[1] - x[0])/D2 ) - (K12/D2);
+		J(1,1) = -d2*invdt*(thetas-thetar)*dS2dpsi2 + dK12dpsi2*(1+(x[1]-x[0])/D2) + (K12/D2) - dK23dpsi2*(1+(x[2]-x[1])/D3)+(K23/D3);
+		J(1,2) = -dK23dpsi3*(1+(x[2]-x[1])/D3)-(K23/D3);
 		//J(2,0) = 0; // Just to remember that this is element of the Jacobian is zero
-		J(2,1) = dK23dpsi2*(1+(psi3-psi2)/D3) - (K12/D3);
-		J(2,2) = -d3*invdt*(thetas-thetar)*dS3dpsi3 + dK23dpsi3*(1+ (psi3-psi2)/D3) + (K23/D3) - d3dxslope*dK3dpsi3 - d3*L*dS3dpsi3;
+		J(2,1) = dK23dpsi2*(1+(x[2]-x[1])/D3) - (K12/D3);
+		J(2,2) = -d3*invdt*(thetas-thetar)*dS3dpsi3 + dK23dpsi3*(1+ (x[2]-x[1])/D3) + (K23/D3) - d3dxslope*dK3dpsi3 - d3*L*dS3dpsi3;
 
         if(!solve(deltax, J, -Fun))
         	cout << "no solution";
         cout <<"x: " <<  x << endl;
         x += deltax;
-        if(x[2]<=thetar || S1>1)
-        	cout << "bad";
         cout << deltax << endl;
         cout << -Fun << endl;
         cout << J << endl;
 
 	    S1 = x[0]<psiae ? 1 : powl(psiae/x[0], 1/lam);
-	    S1 = x[1]<psiae ? 1 : powl(psiae/x[1], 1/lam);
-	    S1 = x[2]<psiae ? 1 : powl(psiae/x[2], 1/lam);
+	    S2 = x[1]<psiae ? 1 : powl(psiae/x[1], 1/lam);
+	    S3 = x[2]<psiae ? 1 : powl(psiae/x[2], 1/lam);
 
 	    theta11 = S1*(thetas - thetar) + thetar;
 	    theta21 = S2*(thetas - thetar) + thetar;
@@ -150,7 +151,7 @@ void Basin::Infilt_Richards(double &f, double &F, double &theta, double &theta1,
    if (k >= MAX_ITER)
    					cout << "WARNING: Max no iterations reached for Richards solution " << endl;
 
-   if(x[2] > thetas){
+/*   if(x[2] > thetas){
 	   x[2]-= (thetas - x[2]);
 	   x[1]+= (thetas - x[2])*d3/d2;
    }
@@ -161,55 +162,60 @@ void Basin::Infilt_Richards(double &f, double &F, double &theta, double &theta1,
    if(x[0] > thetas){
    	   x[0]-= (thetas - x[0]);
    	   pond+= (thetas - x[0])*d1;
-   }
+   }*/
 
-   //calculate average moisture for entire soil profile
-   theta = (d1*x[0] + d2*x[1] + d3*x[2])/depth;
+
 
     F += infilt;
-    f = K1*(1 + (psi1 - pond)/D1 );
-   	theta1 = x[0];
-   	theta2 = x[1];
-   	theta2 = x[2];
+    f = K1*(1 + (x[0] - pond)/D1 );
+   	theta1 =theta11;
+   	theta2 = theta21;
+   	theta3 = theta31;
    	pond -= infilt;
 
+    //calculate average moisture for entire soil profile
+    theta = (d1*theta1 + d2*theta2 + d3*theta3)/depth;
+    double endstor = theta1*d1 + theta2*d2 + theta3*d3;
+
+    double mberr =  (endstor-initstor - infilt*dt + - Qin*dt +  K3*d3dxslope*dt)*100/(endstor + infilt*dt + Qin*dt);
+    cout << mberr;
    switch (flowdir) //add the previously calculated *discharge* (not elevation) to the downstream cell
 	{
 	case 1:
 		_GWupstreamBC->matrix[r + 1][c - 1] += K3*d3dxslope;;
-		_ponding->matrix[r + 1][c - 1] += pond;
+//		_ponding->matrix[r + 1][c - 1] += pond;
 		break;
 	case 2:
 		_GWupstreamBC->matrix[r + 1][c] += K3*d3dxslope;;
-		_ponding->matrix[r + 1][c] += pond;
+//		_ponding->matrix[r + 1][c] += pond;
 		break;
 	case 3:
 		_GWupstreamBC->matrix[r + 1][c + 1] += K3*d3dxslope;;
-		_ponding->matrix[r + 1][c + 1] += pond;
+//		_ponding->matrix[r + 1][c + 1] += pond;
 		break;
 	case 4:
 		_GWupstreamBC->matrix[r][c - 1] += K3*d3dxslope;;
-		_ponding->matrix[r][c - 1] += pond;
+//		_ponding->matrix[r][c - 1] += pond;
 		break;
 	case 5:
 		_dailyGwtrOutput.cells.push_back(cell(r, c, (K3*d3dxslope * _dx)));
-		_dailyOvlndOutput.cells.push_back(cell(r, c, pond * _dx * _dx / dt));
+//		_dailyOvlndOutput.cells.push_back(cell(r, c, pond * _dx * _dx / dt));
 		break; //if it is an outlet store the outflow m3s-1
 	case 6:
 		_GWupstreamBC->matrix[r][c + 1] += K3*d3dxslope;;
-		_ponding->matrix[r][c + 1] += pond;
+//		_ponding->matrix[r][c + 1] += pond;
 		break;
 	case 7:
 		_GWupstreamBC->matrix[r - 1][c - 1] += K3*d3dxslope;;
-		_ponding->matrix[r - 1][c - 1] += pond;
+//		_ponding->matrix[r - 1][c - 1] += pond;
 		break;
 	case 8:
 		_GWupstreamBC->matrix[r - 1][c] += K3*d3dxslope;;
-		_ponding->matrix[r - 1][c] += pond;
+//		_ponding->matrix[r - 1][c] += pond;
 		break;
 	case 9:
 		_GWupstreamBC->matrix[r - 1][c + 1] += K3*d3dxslope;;
-		_ponding->matrix[r - 1][c + 1] += pond;
+//		_ponding->matrix[r - 1][c + 1] += pond;
 		break;
 	default:
 		throw -1;
