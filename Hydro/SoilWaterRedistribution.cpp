c88a6811fb5271c8e84b3239be089b98ccedca45
Marco Maneta <marco.maneta@umontana.edu>
2015-07-22 23:09:21 -0600

    New file for wtaer redistribution code

---
+++ Hydro/SoilWaterRedistribution.cpp
@@ -0,0 +1,15 @@
+/*
+ * SoilWaterRedistribution.cpp
+ *
+ *  Created on: Jul 22, 2015
+ *      Author: marco
+ */
+
+
+#include"Basin.h"
+
+void Basin::SoilWaterRedistribution(double &theta1,
+		double &theta2, double &theta3, double &pond, double &leak, double dt, int r, int c) {
+
+}
+
--- Radiation/SolveSurfaceFluxes.cpp
+++ Radiation/SolveSurfaceFluxes.cpp
@@ -91,6 +91,8 @@ int Basin::SolveSurfaceFluxes(Atmosphere &atm, Control &ctrl) {
 					Infilt_GreenAmpt(infcap, accinf, theta, ponding, gw, dt, r, c); //updates soil moisture
 					_ponding->matrix[r][c] = ponding;
 					_GravityWater->matrix[r][c] = gw;
+
+
 					_BedrockLeakageFlux->matrix[r][c] = leak;
 
 
