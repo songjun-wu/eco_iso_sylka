4dd3320a9bb833d1c0f2bafa28107d5bf5deb18e
Marco Maneta <marco.maneta@umontana.edu>
2015-07-26 15:12:15 -0600

    Implementation of water redistribution equations based on water depth

--- Hydro/SoilWaterRedistribution.cpp
+++ Hydro/SoilWaterRedistribution.cpp
@@ -18,7 +18,7 @@ int Basin::SoilWaterRedistribution(const double &F, double &theta1,
 
 	double K1, K12, K2, K23, K3;
 	double Ss = 0.0;//0005;
-	double thetar = _theta_r->matrix[r][c];
+	double thetafc = _fieldcap->matrix[r][c];
 	double thetas = _porosity->matrix[r][c];
 	double Ks = _Ksat->matrix[r][c];
 	double L = _bedrock_leak->matrix[r][c];
@@ -33,33 +33,34 @@ int Basin::SoilWaterRedistribution(const double &F, double &theta1,
 	double d2 = _depth_layer2->matrix[r][c];
 	double d3 = depth - d1 - d2;
 
-	double dS1dpsi1;
-	double dS2dpsi2;
-	double dS3dpsi3;
-
-	double dK12dpsi1 = 0;
-	double dK12dpsi2 = 0;
-	double dK23dpsi2 = 0;
-	double dK23dpsi3 = 0;
-	double dK3dpsi3 = 0;
+	double dK12dL1 = 0;
+	double dK12dL2 = 0;
+	double dK23dL2 = 0;
+	double dK23dL3 = 0;
+	double dK3dL3 = 0;
 
 	colvec x(3);
 
-	double S1 = (theta1 - thetar) / (thetas - thetar);
-	double S2 = (theta2 - thetar) / (thetas - thetar);
-	double S3 = (theta3 - thetar) / (thetas - thetar);
+	double L1 = theta1*d1;
+	double L2 = theta2*d2;
+	double L3 = theta3*d3;
 
-	double theta11 = S1*(thetas - thetar) + thetar;
-	double theta21 = S2*(thetas - thetar) + thetar;
-	double theta31 = S3*(thetas - thetar) + thetar;
+	double Keff1 = 0;
+	double Keff2 = 0;
+	double Keff3 = 0;
 
-    x[0] = psiae*powl(S1,-lam);
-    x[1] = psiae*powl(S2,-lam);
-    x[2] = psiae*powl(S3,-lam);
+    x[0] = L1;
+    x[1] = L2;
+    x[2] = L3;
 
-    double xi0 = x[0];
-    double xi1 = x[1];
-    double xi2 = x[2];
+    double S1 =std::max<double>(0, (L1/d1 - thetafc)/(thetas - thetafc));
+    double S2 = std::max<double>(0, (L2/d2 - thetafc)/(thetas - thetafc));
+    double S3 = std::max<double>(0, (L3/d3 - thetafc)/(thetas - thetafc));
+
+
+	double dS1L1 = 1/(d1*(thetas - thetafc));
+	double dS2L2 = 1/(d2*(thetas - thetafc));
+	double dS3L3 = 1/(d3*(thetas - thetafc));
 
 	colvec Fun(3);
 	mat J = zeros < mat > (3, 3);
@@ -69,40 +70,47 @@ int Basin::SoilWaterRedistribution(const double &F, double &theta1,
 
 	do {
 
-	K1 = Ks * powl(S1, p);
-	K2 = Ks * powl(S2, p);
-	K3 = Ks * powl(S3, p);
-	K12 =  (d1 * K1 + d2 * K2)/(d1 + d2);
-	K23 =  (d2 * K2 + d3 * K3)/(d2 + d3);
+	Keff1 = std::fabs(x[0] - thetafc*d1)/dt;
+	Keff2 = std::fabs(x[1] - thetafc*d2)/dt;
+	Keff3 = std::fabs(x[2] - thetafc*d3)/dt;
+
+	K1 = std::min<double>(Ks * powl(S1, p), Keff1);
+	K2 = std::min<double>(Ks * powl(S2, p), Keff2);
+	K3 = std::min<double>(Ks * powl(S3, p), Keff3);
+	K12 =  std::min<double>((d1 * K1 + d2 * K2)/(d1 + d2), Keff1);
+	K23 =  std::min<double>((d2 * K2 + d3 * K3)/(d2 + d3), Keff1);
+
+	dK12dL1 =  S1> 1 ? 0 : Ks*d1/(d1+d2)*p*powl(S1,p-1)*dS1L1;
+	dK12dL2 =  S2> 1 ? 0 : Ks*d2/(d1+d2)*p*powl(S2,p-1)*dS2L2;
+	dK23dL2 =  S2> 1 ? 0 : Ks*d2/(d2+d3)*p*powl(S2,p-1)*dS2L2;
+	dK23dL3 =  S3> 1 ? 0 : Ks*d3/(d2+d3)*p*powl(S3,p-1)*dS3L3;
+	dK3dL3  =  S3> 1 ? 0 : Ks*p*powl(S3,p-1)*dS3L3;
+
+	if(K1 == Keff1)
+		dK12dL1 = 1/dt;
+	if(K2 == Keff2){
+		dK12dL2 = 1/dt;
+		dK23dL2 = 1/dt;}
+	if(K3 == Keff3){
+		dK23dL3 = 1/dt;
+		dK3dL3 = 1/dt;}
 
 
-	dS1dpsi1 = x[0]<psiae ? 0 : -powl(psiae/x[0],1/lam)/(lam*x[0]);
-	dS2dpsi2 = x[1]<psiae ? 0 : -powl(psiae/x[1],1/lam)/(lam*x[1]);
-	dS3dpsi3 = x[2]<psiae ? 0 : -powl(psiae/x[2],1/lam)/(lam*x[2]);
-
-
-	dK12dpsi1 =  Ks*d1/(d1+d2)*p*powl(S1,p-1)*dS1dpsi1;
-	dK12dpsi2 =  Ks*d2/(d1+d2)*p*powl(S2,p-1)*dS2dpsi2;
-	dK23dpsi2 =  Ks*d2/(d2+d3)*p*powl(S2,p-1)*dS2dpsi2;
-	dK23dpsi3 =  Ks*d3/(d2+d3)*p*powl(S3,p-1)*dS3dpsi3;
-	dK3dpsi3  =  Ks*p*powl(S3,p-1)*dS3dpsi3;
-
-
-	Fun[0] = theta11 - theta1 - S1*Ss*(x[0] -  xi0) - (F  - K12 * dt) / d1;
-	Fun[1] = theta21 - theta2 - S2*Ss*(x[1] -  xi1) - (K12 - K23) * dt / d2;
-	Fun[2] = theta31 - theta3 - S3*Ss*(x[2] -  xi2)  - (K23 - L * K3) * dt / d3;
+	Fun[0] = x[0] - L1  - (F  - K12 * dt);
+	Fun[1] = x[1] - L2  - (K12 - K23) * dt;
+	Fun[2] = x[2] - L3  - (K23 - L * K3) * dt;
 
 	cout << "Fun: " << Fun << endl;
 
-	J(0, 0) = dS1dpsi1*(thetas - thetar)  - dS1dpsi1*Ss*x[0] - S1*Ss + dt / d1 * dK12dpsi1;
-	J(0, 1) = dt / d1 * dK12dpsi2;
+	J(0, 0) = 1 + dt * dK12dL1;
+	J(0, 1) = dt * dK12dL2;
 	J(0, 2) = 0;
-	J(1, 0) = -dt / d2 * dK12dpsi1;
-	J(1, 1) = dS2dpsi2*(thetas - thetar)  -dS2dpsi2* Ss*x[1] - S2*Ss - dt / d2 * (dK12dpsi2 - dK23dpsi2);
-	J(1, 2) = dt / d2 * dK23dpsi3;
+	J(1, 0) = -dt  * dK12dL1;
+	J(1, 1) = 1 - dt * (dK12dL2 - dK23dL2);
+	J(1, 2) = dt * dK23dL3;
 	J(2, 0) = 0;
-	J(2, 1) = -dt / d3 * dK23dpsi2;
-	J(2, 2) = dS3dpsi3*(thetas - thetar) - dS3dpsi3*Ss*x[2] - S2*Ss - dt / d3 * (dK23dpsi3 - L * dK3dpsi3);
+	J(2, 1) = -dt * dK23dL2;
+	J(2, 2) = 1 - dt * (dK23dL3 - L * dK3dL3);
     cout << "J" << J << endl;
 
 	if (!solve(deltax, J, -Fun)) {
@@ -112,25 +120,24 @@ int Basin::SoilWaterRedistribution(const double &F, double &theta1,
 	//	        cout <<"x: " <<  x << endl;
 	x += deltax;
 
-	S1 = x[0]<psiae ? 1 : powl(psiae/x[0], 1/lam);
-	S2 = x[1]<psiae ? 1 : powl(psiae/x[1], 1/lam);
-	S3 = x[2]<psiae ? 1 : powl(psiae/x[2], 1/lam);
-
-
-	theta11 = S1*(thetas - thetar) + thetar;// + std::max<double>(0, 0.005*(psiae - x[0]) );
-	theta21 = S2*(thetas - thetar) + thetar;// +std::max<double>(0, 0.005*(psiae - x[1]) );
-	theta31 = S3*(thetas - thetar) + thetar;// + std::max<double>(0, 0.005*(psiae - x[2]) );
+	S1 = std::max<double>(0,(x[0]/d1 - thetafc)/(thetas - thetafc));
+	S2 =std::max<double>(0, (x[1]/d2 - thetafc)/(thetas - thetafc));
+	S3 = std::max<double>(0,(x[2]/d3 - thetafc)/(thetas - thetafc));
 
 	k++;
 	cout << x << endl << deltax << endl;
 	} while (norm(deltax, 2) > 0.00000001 && k < MAX_ITER);
 
 
-		theta1 = theta11;
-		theta2 = theta21;
-		theta3 = theta31;
+		theta1 = x[0]/d1;
+		theta2 = x[1]/d2;
+		theta3 = x[2]/d3;
 
-		K3 = Ks * powl(S3, p);
+		if(theta1>thetas){
+			pond += -(thetas - theta1) * d1;
+			theta1 = thetas;}
+
+		K3 = Ks * powl(std::max<double>(0,S3), p);
         leak =   L * K3;
 }
 
