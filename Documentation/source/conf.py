070e1920f2847f8b64c57dc42c0882eefe8d447e
Marco Maneta <marco.maneta@umontana.edu>
2016-08-02 01:21:10 -0600

    changed theme for rendering docs to default

--- Documentation/source/conf.py
+++ Documentation/source/conf.py
@@ -31,7 +31,7 @@ import os
 extensions = ['sphinx.ext.autodoc','sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['ntemplates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -108,7 +108,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
